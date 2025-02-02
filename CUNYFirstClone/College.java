4dd34cb4b2187711548ec367dcc36928ea91b846
alfredscyriac <alfredscyriac@gmail.com>
2025-02-02 15:06:18 -0500

    feat: Added constructor and implemented common methods in abstract class - College

--- CUNYFirstClone/College.java
+++ CUNYFirstClone/College.java
@@ -1,5 +1,10 @@
 package CUNYFirstClone;
 
 public abstract class College {
-    
+    protected String CollegeName;
+    protected String CollegeLocation; 
+    public College(String CollegeName, String CollegeLocation){
+        this.CollegeName=CollegeName; 
+        this.CollegeLocation=CollegeLocation; 
+    } 
 }
