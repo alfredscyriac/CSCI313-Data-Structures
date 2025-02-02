03106436c68f6329007f8aee78becd22efb2c511
alfredscyriac <alfredscyriac@gmail.com>
2025-02-02 15:45:16 -0500

    feat: constructor for csdepartment class

--- CUNYFirstClone/CSDepartment.java
+++ CUNYFirstClone/CSDepartment.java
@@ -1,5 +1,11 @@
 package CUNYFirstClone;
 
 public class CSDepartment extends QueensCollege {
-    
+    private String departmentHead;
+    public CSDepartment(String departmentHead) {
+        this.departmentHead = departmentHead;
+    }
+    public void printDepartmentInfo(){
+        super.println("Department Head: " + this.departmentHead);
+    }
 }
