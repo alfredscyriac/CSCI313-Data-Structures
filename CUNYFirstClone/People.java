5e1580d722555f837a7cc539a4087dd0f258840f
alfredscyriac <alfredscyriac@gmail.com>
2025-02-02 22:13:10 -0500

    feat: constructor for abstract class people that just has name and age

--- CUNYFirstClone/People.java
+++ CUNYFirstClone/People.java
@@ -1,5 +1,10 @@
 package CUNYFirstClone;
 
 public abstract class People {
-    
+    protected String name; 
+    protected int age; 
+    public People(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
