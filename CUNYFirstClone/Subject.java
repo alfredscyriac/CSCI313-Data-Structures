9613479b285e7595487a3672e7f38fe360ef4c63
alfredscyriac <alfredscyriac@gmail.com>
2025-02-02 22:11:04 -0500

    feat: Constructor and methods for Subject class that has subject name and professor name

--- CUNYFirstClone/Subject.java
+++ CUNYFirstClone/Subject.java
@@ -1,5 +1,19 @@
 package CUNYFirstClone;
 
 public class Subject {
-    
+    private String subjectName;
+    private String professorName; 
+    public Subject(String subjectName, String professorName) {
+        this.subjectName = subjectName;
+        this.professorName = professorName;
+    }
+    public String getSubjectName(){
+        return subjectName; 
+    }
+    public String getProfessorNme(){
+        return professorName; 
+    }
+    public void displaySubjectInfo() {
+        System.out.println("Subject: " + subjectName + ", Taught by: Prof. " + professorName);
+    }
 }
