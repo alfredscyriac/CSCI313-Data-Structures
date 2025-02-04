9f6a5d699b6792467cbcf30ad97b68c0e4e35767
alfredscyriac <alfredscyriac@gmail.com>
2025-02-04 09:58:44 -0500

    fix: debug errors in methods

--- day_02_arrays/ExpendableArray.java
+++ day_02_arrays/ExpendableArray.java
@@ -6,25 +6,27 @@ public class ExpendableArray {
 	private String[] array;
 	
 	public ExpendableArray(){//default constructor
-		size=0; 
-		capacity=5; 
-		array= new String[capacity];
+		this.size=0; 
+		this.capacity=5; 
+		this.array = new String[capacity];
+		this.fillArray();
 	}
 	
 	public ExpendableArray(int cap){
-		size=0; 
-		capacity=cap; 
-		array= new String[capacity];
+		this.size=0; 
+		this.capacity=cap; 
+		this.array = new String[capacity];
+		this.fillArray();
 	}
 	
 	private void fillArray() {
-		for(int i=0; i<capacity; i++){
-			array[i]=null; 
+		for(int i=0; i<this.capacity; i++){
+			this.array[i]=null; 
 		}
 	}
 
 	private boolean isAlmostFull() {
-		if(size>=capacity/2){
+		if((100.0*size)/capacity>=70.0){
 			return true; 
 		}
 		else{
@@ -33,28 +35,34 @@ public class ExpendableArray {
 	}
 	
 	private boolean isAlmostEmpty() {
-		if(size<capacity/2){
+		if((100.0*size)/capacity<=30.0){
 			return true; 
 		}
 		else{
 			return false; 
-		}	
+		}
 	}
 	
 	private void doubleCapacity() {
-		
+		this.capacity*=2; 
+		this.copyArray();
 	}
 	
 	private void halveCapacity() {
-		
+		this.capacity=Math.max(5,this.capacity/2);
+		this.copyArray(); 
 	}
 	
 	private void copyArray() {
-		
+		String[] tempArray = new String[capacity]; 
+		for(int i=0; i<this.capacity;i++){
+			tempArray[i]=this.array[i];
+		}
+		this.array=tempArray; 
 	}
 	
 	public String get(int index) {
-		return null;
+		return this.array[index]; 
 	}
 	
 	public void add(String e) {
