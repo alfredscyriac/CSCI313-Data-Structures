e289ed28cc24baeeb098a23e5d0dc1f245cb969e
alfredscyriac <alfredscyriac@gmail.com>
2025-02-10 23:37:00 -0500

    feat: insertion sort algorithm

--- day_02_arrays/InsertionSortAlgo.java
+++ day_02_arrays/InsertionSortAlgo.java
@@ -14,7 +14,20 @@ public class InsertionSortAlgo {
     }
 
     static void insertionSort(int[] arr) {
-        
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i]; // Current element to be inserted in sorted part
+            int j = i - 1;
+
+            // Shift elements of the sorted part to the right to make space for key
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            // Place key at its correct position
+            arr[j + 1] = key;
+        }
     }
 
     static void printArray(int[] arr) {
