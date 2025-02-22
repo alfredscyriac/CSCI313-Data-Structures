ce3919d1aa18c98a9750a0e0d8dc3959336e977c
Alfred <alfredscyriac@gmail.com>
2025-02-22 09:55:19 -0500

    Stack constructors and methods utilizing LinkedList

--- day_05_starter/ListStack.java
+++ day_05_starter/ListStack.java
@@ -5,36 +5,49 @@ public class ListStack {
 	private LinkedList<String> stack;
 	
 	public ListStack() {
-		
+		stack = new LinkedList<>();
 	}
 	
 	/**
 	 * Places an element at the top of the stack.
-	 * What is the runtime?
+	 * What is the runtime? 
+	 * Runtime: O(1)
 	 * @param element
 	 */
 	public void push(String element) {
-		
+		stack.addFirst(element);
 	}
 	
 	/**
 	 * Removes the element at the top of the stack.
 	 * If stack is empty, it returns null.
 	 * What is the runtime?
+	 * Runtime: O(1)
 	 * @return String
 	 */
 	public String pop() {
-		
+		if(stack.isEmpty()){
+			return null;
+		}
+		else{ 
+			String temp = stack.getFirst();
+			stack.removeFirst();
+			return temp;
+		}
 	}
 	
 	/**
 	 * Returns but does not remove the element at the top of the stack.
 	 * If stack is empty, it returns null.
 	 * What is the runtime?
+	 * Runtime: O(1)
 	 * @return String
 	 */
 	public String peek() {
-		
+		if(stack.isEmpty()){
+			return null;
+		}
+		return stack.getFirst();
 	}
 	
 	public boolean isEmpty() {
@@ -42,6 +55,6 @@ public class ListStack {
 	}
 	
 	public int size() {
-		return stack.getLength();
+		return stack.size();
 	}
 }
