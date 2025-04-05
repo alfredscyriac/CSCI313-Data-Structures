0c07e3a9fd3496f616a3358810342457cd75b24b
Alfred <alfredscyriac@gmail.com>
2025-04-05 10:24:37 -0400

    constructor, setters, and getters for Node class

--- exam2_hashmap/Node.java
+++ exam2_hashmap/Node.java
@@ -1,5 +1,27 @@
 package exam2_hashmap;
 
-public class Node {
-    
+public class Node<Pair> {
+    private Pair data; 
+    private Node<Pair> next; 
+
+    public Node(Pair data) {
+        this.data = data; 
+        this.next = null; 
+    }
+
+    public Pair getData() {
+        return data; 
+    }
+
+    public void setData(Pair data) {
+        this.data = data; 
+    }
+
+    public Node<Pair> getNext() {
+        return next; 
+    }
+
+    public void setNext(Node<Pair> next) {
+        this.next = next; 
+    }    
 }
