92f795450f8f71b9e9384ab5a445da0a53d5d632
Alfred <alfredscyriac@gmail.com>
2025-04-05 14:55:47 -0400

    default constructor and custom constructor for ChainHashMap class

--- exam2_hashmap/ChainHashMap.java
+++ exam2_hashmap/ChainHashMap.java
@@ -1,5 +1,29 @@
 package exam2_hashmap;
 
-public class ChainHashMap {
+public class ChainHashMap<K,V> {
+    private int capacity = 101; 
+    private LinkedList<Pair<K,V>>[] arr; 
+    private int size; 
+
+    // Default constructor 
+    @SuppressWarnings("unchecked")
+    public ChainHashMap() {
+        arr = (LinkedList<Pair<K, V>>[]) new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            arr[i] = new LinkedList<Pair<K, V>>();
+        }
+        size = 0;
+    }
+
+    // Custom capacity constructor 
+    @SuppressWarnings("unchecked")
+    public ChainHashMap(int customcapacity) {
+        arr = (LinkedList<Pair<K, V>>[]) new LinkedList[customcapacity];
+        for (int i = 0; i < customcapacity; i++) {
+            arr[i] = new LinkedList<Pair<K, V>>();
+        }
+        size = 0;
+    }
+
     
 }
