88d3476c9f84a8004f868b99a5c55954100cab77
Alfred <alfredscyriac@gmail.com>
2025-04-05 17:08:40 -0400

    create OpenAddressHashMap class, create instance variables, create constructor, function title lines for all desired methods

---
+++ exam2_hashmap/OpenAddressHashMap.java
@@ -0,0 +1,39 @@
+package exam2_hashmap;
+
+public class OpenAddressHashMap<K,V> {
+    int capacity = 101; 
+    Pair<K,V>[] bucket; 
+    int size; 
+
+    public OpenAddressHashMap() {
+        bucket = new Pair[capacity]; 
+        size = 0; 
+    }
+
+    public int hashfunction(K key) {
+
+    }
+
+    public void put(Pair<K,V> pair) {
+
+    }
+
+    public Pair<K,V> get(K key) {
+
+    }
+
+    public Pair<K,V> remove(K key) {
+
+    }
+
+    public LinkedList<K> keySet() {
+
+    }
+
+    public LinkedList<Pair<K,V>> entrySet() {
+        
+    }
+
+
+    
+}
