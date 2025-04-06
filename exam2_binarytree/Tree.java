8b5de09331a539c2d999c181f6db8a47b0524227
Alfred <alfredscyriac@gmail.com>
2025-04-06 14:58:51 -0400

    import: binary tree class starter code with function title lines for all desired methods

--- exam2_binarytree/Tree.java
+++ exam2_binarytree/Tree.java
@@ -1,5 +1,44 @@
 package exam2_binarytree;
 
-public class Tree {
-    
+public class Tree<E> {
+	
+	Node<E> root;
+	int size;
+	
+	public E element(Node<E> node) {
+		
+	}
+	
+	public Node<E> getParent(Node<E> node) {
+		
+	}
+	
+	public LinkedList<Node<E>> getChildren(Node<E> node) {
+		
+	}
+	
+	
+	public boolean isRoot(Node<E> node) {
+		
+	}
+	
+	public boolean isExternal(Node<E> node) {
+		
+	}
+	
+	public boolean isInternal(Node<E> node) {
+		
+	}
+	
+	public int nodeDepth(Node<E> node) {
+		
+	}
+	
+	public int treeHeight() {
+		
+	}
+	
+	public int size() {
+		
+	}
 }
